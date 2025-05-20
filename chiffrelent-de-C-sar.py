0ff92ee840bba8d6fa2f8b7e2e14f8bb76e9ecc3
Test <kilianberteaudca@gmail.com>
2025-05-20 14:15:25 -0400

    Premier essai

--- chiffrelent-de-C-sar.py
+++ chiffrelent-de-C-sar.py
@@ -1,5 +1,7 @@
 import os
+# Module pour enlever les caracères spéciaux
 from enlever_caracteres import lire_liste_mots, enlever_caracteres_speciaux
+
 def ouverture_fichier(dossier, fichier="message.txt"):
     # teste si le fichier existe
     full_filename = os.path.join(dossier,fichier)
@@ -10,6 +12,9 @@ def ouverture_fichier(dossier, fichier="message.txt"):
     with open("message.txt", "r", encoding = "utf-8") as fio:
 # Lire le contenu du fichier
         contenu = fio.read()
+        for lettre_avec_accent in contenu[-6:]:
+            lettre_sans_accent = enlever_caracteres_speciaux(lettre_avec_accent)
+            print(f'    {lettre_avec_accent} -> {lettre_sans_accent}')
 
 # On crée un fichier txt et on écrit dedans
 with open("message_encrypte.txt", "w") as fio:
